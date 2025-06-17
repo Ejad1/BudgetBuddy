3909f210c742b01094360d12dbbee87edb103403
Ejad1 <edoumeyan@gmail.com>
2025-06-17 18:48:04 +0000

    Mise à jour du lien de l'api Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://budgetbuddy-1-aafa.onrender.com',
         changeOrigin: true,
         secure: false,
       },
